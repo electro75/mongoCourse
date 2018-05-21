05571d390761b79eceb00a3597d903cf0d1f1960
sahil n <sahil@mutterfly.in>
2018-05-21 17:53:31 +0530

    optimises connection logic and also adds a default promise declaration

--- test/testHelper.js
+++ test/testHelper.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection
-        .once('open', ()=>{ console.log('Success'); })
-        .on('error', (error)=>{ console.warn('Warning', error) 
-});
+mongoose.Promise = global.Promise;      //Commands mongoose to use Es6 promises.
+
+before((done)=>{                        //Commands Mocha to pause excection untill mongoose has successfully connected to Mongo.
+        mongoose.connect('mongodb://localhost/users_test');
+        mongoose.connection
+                .once('open', ()=>{ done(); })
+                .on('error', (error)=>{ console.warn('Warning', error) 
+        });
+})
+
 
 beforeEach((done)=>{
         mongoose.connection.collections.users.drop(()=>{
