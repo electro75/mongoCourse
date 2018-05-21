c446dba61b68da0676e5a7f583f0425fdafb0600
sahil n <sahil@mutterfly.in>
2018-05-21 17:27:09 +0530

    adds a beforeEach snippet to clear out database before running a test

--- test/testHelper.js
+++ test/testHelper.js
@@ -3,4 +3,12 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/users_test');
 mongoose.connection
         .once('open', ()=>{ console.log('Success'); })
-        .on('error', (error)=>{ console.warn('Warning', error) });
+        .on('error', (error)=>{ console.warn('Warning', error) 
+});
+
+beforeEach((done)=>{
+        mongoose.connection.collections.users.drop(()=>{
+        // Ready to run the next test.
+                done();
+        });
+})
