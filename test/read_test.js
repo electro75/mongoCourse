f60f1d07bb506fabbf00a47c4851837e1ad57c1c
sahil n <sahil@mutterfly.in>
2018-05-22 12:44:14 +0530

    adds test file for testing find query.

---
+++ test/read_test.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Reading Users from database', ()=>{
+    let john;
+
+    beforeEach((done)=>{
+        john = new User({name: 'John'});
+
+        john.save()
+            .then(()=>{ done(); })
+    })
+
+    it('Finds all Users with name of John', (done)=>{
+        User.find({name: 'John'})
+            .then((users)=>{
+                console.log(users)
+                done();
+            })
+    })
+})
