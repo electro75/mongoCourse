a86a0e3b6523dcb6b380d1d7ceb4e1e1a1566147
sahil n <sahil@mutterfly.in>
2018-05-25 12:16:12 +0530

    adds validators to the name prop.

--- test/validation_test.js
+++ test/validation_test.js
@@ -3,7 +3,19 @@ const User = require('../src/user');
 
 describe('validating records', ()=>{
 
-    it('requires a user name', ()=>{
-        
+    it('requires user to have a name', ()=>{
+        const user = new User({name: undefined});       //Name is explicitly set to undefined to test the validation.
+        const validationRes = user.validateSync();
+        const { message } =  validationRes.errors.name;  
+
+        assert(message === 'Name is required.');
     });
+
+    it('requires the name to be longer than 2 characters', ()=>{
+        const user = new User({name: 'Al'});            //Name is explicitly set to Al to test the validator function.
+        const validationRes = user.validateSync();
+        const { message } = validationRes.errors.name;
+
+        assert( message === 'Name must be longer than 2 characters' );
+    })
 });
