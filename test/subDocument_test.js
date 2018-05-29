9e835f1553d9a2e72bb11e7445b05b32385aae29
sahil n <sahil@mutterfly.in>
2018-05-29 17:30:34 +0530

    adds test for testing subdocuments

--- test/subDocument_test.js
+++ test/subDocument_test.js
@@ -1,12 +1,23 @@
 const assert = require('assert');
 const User = require('../src/user');
 
-describe('Sub Documents', ()=>{
+describe('Sub Documents', (done)=>{
     it('can create a subdocument', (done)=>{
         const john = new User({
             name: 'John',
             posts: [{ title: 'PostTitle'}]
         });
 
+        john.save()
+            .then(() => User.findOne({ name: 'John'}))
+            .then((user) => {
+                assert(user.posts[0].title === 'PostTitle')
+                done();
+            })
+
+    });
+
+    it('Can add subdocuments to an existing record', (done)=>{
+        
     })
 });
