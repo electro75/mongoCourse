34eb1a75c5f273e00e73bbd136d0fada21bc69d3
sahil n <sahil@mutterfly.in>
2018-05-31 18:26:16 +0530

    adds getter function. (incomplete)

--- src/user.js
+++ src/user.js
@@ -14,6 +14,10 @@ const UserSchema = new Schema({                         // creates the schema
     posts: [ PostSchema ]
 });
 
+UserSchema.virtual('postCount').get(function() {
+    
+})
+
 const User = mongoose.model('user', UserSchema);        // creates the model
 
 module.exports = User;                                  // exports the model
