5655e0941dfd387845878f30d60e337f34029d84
sahil n <sahil@mutterfly.in>
2018-06-01 21:20:21 +0530

    adds test to fetch entire object graph

--- test/association_test.js
+++ test/association_test.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const assert = require('assert');
+
 const User = require('../src/user');
 const Comment = require('../src/comment');
 const BlogPost = require('../src/blogPost');
@@ -25,22 +27,48 @@ describe('Associations', ()=>{
             comment is not a 'collection' it is directly
             equated to user instance and mongoose handles 
             the rest.*/
-
         comment.user = joe;
 
         // below line takes array of promises and executes .then 
         // only when all have been settled. Promises are executed 
         // in parallel.
-
         Promise.all([ joe.save(), blogPost.save(), comment.save() ])        
                .then(()=> done());
     });
 
-    it.only('saves a relation between a user and a blogpost', (done)=>{
+
+    // it.only is used to only run a particular test.
+    it('saves a relation between a user and a blogpost', (done)=>{
         User.findOne({name: 'Joe'})
+            .populate('blogPosts')
             .then((user)=>{
-                console.log(user);
+                assert(user.blogPosts[0].title === 'JS is fun.');
                 done();
-            });
+            }
+        );
     });
+
+    it('saves a full relation graph', (done)=>{
+        User.findOne({name: 'Joe'})
+            .populate({
+                path: 'blogPosts',
+                populate: {
+                    path: 'comments',
+                    model: 'comment',
+                    populate: {
+                        path: 'user',
+                        model: 'user'
+                    }   
+                }
+            }
+        )
+        .then((user)=>{
+            assert(user.name === 'Joe');
+            assert(user.blogPosts[0].title === 'JS is fun.');
+            assert(user.blogPosts[0].comments[0].content === 'This is my first post btw.');
+            assert(user.blogPosts[0].comments[0].user.name === 'Joe');
+
+            done();
+        })
+    })
 });
